d253c52173b50e397269620382e1545979697508
Maligosus <iceslaer@icloud.com>
2026-03-02 03:04:38 +0500

    chore: set headshot only basic zomibe

--- RE9/reframework/autorun/headshot_kills.lua
+++ RE9/reframework/autorun/headshot_kills.lua
@@ -9,8 +9,16 @@ local head_body_parts_set = Set:new({
     1
 })
 
+local zombie_prefix_name = 'cp_B0'
+
 local is_headshot_in_context = false
 
+-- Zombies
+-- cp_B070
+-- cp_B001
+-- cp_B000
+
+-- cp_D100 Mr Gideon
 
 do
     local hit_manager_singleton = sdk.get_managed_singleton('app.HitManager')
@@ -35,16 +43,38 @@ do
             local damage_user_data = sdk.to_managed_object(hit_info:get_field('<DamageUserData>k__BackingField'))
             local attack_data = sdk.to_managed_object(hit_info:get_field('<AttackData>k__BackingField'))
 
+
+            if args[4] == nil then
+                log.debug('args4 is null')
+            end
+
             if damage_user_data == nil or attack_data == nil then
                 log.debug('damaga user data nil')
                 return
             end
 
-            local body_parts = damage_user_data:get_field('_BodyParts')
+            local damage_hit_controller = sdk.to_managed_object(hit_info:get_field(
+                '<DamageHitController>k__BackingField'))
 
-            if head_body_parts_set:has(body_parts) then
-                log.debug('ITS HEAD')
-                log.debug('BodyParts= ' .. tostring(damage_user_data:get_field('_BodyParts')))
+            if damage_hit_controller == nil then
+                log.debug('NO DAMAGE HIT CONTROLLER')
+            end
+
+            local attack_owner = sdk.to_managed_object(damage_hit_controller:call('get_AttackOwner()'))
+
+            if attack_owner == nil then
+                log.debug('Attack Owner not exists')
+                return
+            end
+
+
+            local body_parts = damage_user_data:get_field('_BodyParts')
+            ---@type string
+            local enemy_name = attack_owner:call('get_Name()')
+
+            local is_enemy_basic_zombie = enemy_name:match(zombie_prefix_name)
+
+            if head_body_parts_set:has(body_parts) and is_enemy_basic_zombie then
                 is_headshot_in_context = true
             end
         end,
